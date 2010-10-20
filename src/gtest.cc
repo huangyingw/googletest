61b83123bddddd5cdc95631f0d9ee8776dc1c1a9
huangyingw@linux_programming <huangyingw@gmail.com>
2010-10-20 17:53:34 -0400

    n

--- src/gtest.cc
+++ src/gtest.cc
@@ -2185,6 +2185,7 @@ TestInfo* MakeAndRegisterTestInfo(
   TestInfo* const test_info =
       new TestInfo(test_case_name, name, test_case_comment, comment,
                    fixture_class_id, factory);
+  std::cout<<test_case_name<<std::endl;
   GetUnitTestImpl()->AddTestInfo(set_up_tc, tear_down_tc, test_info);
   return test_info;
 }
