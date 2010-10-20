bff31e212eab8bd2fc67c7ed2723163763cbbed9
huangyingw@linux_programming <huangyingw@gmail.com>
2010-10-20 17:08:30 -0400

    n

--- samples/sample1_unittest.cc
+++ samples/sample1_unittest.cc
@@ -7,35 +7,3 @@ TEST(FactorialTest, Negative) {
   EXPECT_EQ(1, Factorial(-1));
   EXPECT_TRUE(Factorial(-10) > 0);
 }
-
-TEST(FactorialTest, Zero) {
-  EXPECT_EQ(1, Factorial(0));
-}
-
-TEST(FactorialTest, Positive) {
-  EXPECT_EQ(1, Factorial(1));
-  EXPECT_EQ(2, Factorial(2));
-  EXPECT_EQ(6, Factorial(3));
-  EXPECT_EQ(40320, Factorial(8));
-}
-
-TEST(IsPrimeTest, Negative) {
-
-  EXPECT_FALSE(IsPrime(-1));
-  EXPECT_FALSE(IsPrime(-2));
-  EXPECT_FALSE(IsPrime(INT_MIN));
-}
-
-TEST(IsPrimeTest, Trivial) {
-  EXPECT_FALSE(IsPrime(0));
-  EXPECT_FALSE(IsPrime(1));
-  EXPECT_TRUE(IsPrime(2));
-  EXPECT_TRUE(IsPrime(3));
-}
-
-TEST(IsPrimeTest, Positive) {
-  EXPECT_FALSE(IsPrime(4));
-  EXPECT_TRUE(IsPrime(5));
-  EXPECT_FALSE(IsPrime(6));
-  EXPECT_TRUE(IsPrime(23));
-}
