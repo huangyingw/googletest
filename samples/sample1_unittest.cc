0c39df14a350c58b692ecd57ca5950abc0f40563
huangyingw@linux_programming <huangyingw@gmail.com>
2010-10-20 17:04:58 -0400

    n

--- samples/sample1_unittest.cc
+++ samples/sample1_unittest.cc
@@ -33,22 +33,9 @@ TEST(IsPrimeTest, Trivial) {
   EXPECT_TRUE(IsPrime(3));
 }
 
-// Tests positive input.
 TEST(IsPrimeTest, Positive) {
   EXPECT_FALSE(IsPrime(4));
   EXPECT_TRUE(IsPrime(5));
   EXPECT_FALSE(IsPrime(6));
   EXPECT_TRUE(IsPrime(23));
 }
-
-// Step 3. Call RUN_ALL_TESTS() in main().
-//
-// We do this by linking in src/gtest_main.cc file, which consists of
-// a main() function which calls RUN_ALL_TESTS() for us.
-//
-// This runs all the tests you've defined, prints the result, and
-// returns 0 if successful, or 1 otherwise.
-//
-// Did you notice that we didn't register the tests?  The
-// RUN_ALL_TESTS() macro magically knows about all the tests we
-// defined.  Isn't this convenient?
