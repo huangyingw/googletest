dc25fde0ec642f0f809fb1d7a538dda419009c85
huangyingw@linux_programming <huangyingw@gmail.com>
2010-10-25 11:41:11 +0800

    n

--- samples/sample1_unittest.cc
+++ samples/sample1_unittest.cc
@@ -6,3 +6,8 @@ TEST(FactorialTest, Negative) {
   EXPECT_EQ(1, Factorial(-1));
   EXPECT_TRUE(Factorial(-10) > 0);
 }
+TEST(Factorial, NonNegative) {
+ EXPECT_EQ(1, Factorial(-5));
+  EXPECT_EQ(1, Factorial(-1));
+  EXPECT_TRUE(Factorial(-10) > 0);
+}
