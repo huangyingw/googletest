99ad462082fa7d5a20524245596bae9c59758126
huangyingw@linux_programming <huangyingw@gmail.com>
2010-10-25 11:44:21 +0800

    n

--- src/gtest.cc
+++ src/gtest.cc
@@ -3518,7 +3518,6 @@ const TestCase* UnitTest::GetTestCase(int i) const {
 // Gets the i-th test case among all the test cases. i can range from 0 to
 // total_test_case_count() - 1. If i is not in that range, returns NULL.
 TestCase* UnitTest::GetMutableTestCase(int i) {
-  std::cout<<i<<std::endl;
   return impl()->GetMutableTestCase(i);
 }
 
@@ -3960,6 +3959,7 @@ bool UnitTestImpl::RunAllTests() {
       // Runs the tests only if there was no fatal failure during global
       // set-up.
       if (!Test::HasFatalFailure()) {
+        std::cout<<"total_test_case_count:->"<<total_test_case_count()<<std::endl;
         for (int test_index = 0; test_index < total_test_case_count();
              test_index++) {
           GetMutableTestCase(test_index)->Run();
